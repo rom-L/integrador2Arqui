ca6362941902766de98bce36d5accd055f56ae19
rom-L <larrondoromeo@gmail.com>
2023-10-06 19:48:08 -0300

    creo interfaz repoEstudiante

--- interfaces/RepoEstudianteCarrera.java
+++ interfaces/RepoEstudianteCarrera.java
@@ -1,7 +1,17 @@
 package integrador2Arqui.interfaces;
 
-import integrador2Arqui.clases.Carrera;
 
 public interface RepoEstudianteCarrera {
-
+	//matricular un estudiante en una carrera
+	//recuperar las carreras con estudiantes inscriptos, y ordenar por cantidad de inscriptos
+	//recuperar los estudiantes de una determinada carrera, filtrado por ciudad de residencia
+	
+	/*
+	public void insert(Estudiante estudiante);
+	public estudianteDTO getAll();
+	public estudianteDTO getByLibreta(int id);
+	public estudianteDTO getByGenero(String genero);
+	public boolean delete(int id);
+	public boolean update(Estudiante estudiante);
+	*/
 }
