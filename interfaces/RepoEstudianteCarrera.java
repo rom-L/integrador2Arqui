52e68c80ed163248cdb1734057e2099b517c81a2
rom-L <larrondoromeo@gmail.com>
2023-10-06 20:45:15 -0300

    fix interfaces

--- interfaces/RepoEstudianteCarrera.java
+++ interfaces/RepoEstudianteCarrera.java
@@ -2,6 +2,7 @@ package integrador2Arqui.interfaces;
 
 import integrador2Arqui.DTO.CarreraConInscriptosDTO;
 import integrador2Arqui.DTO.EstudianteDTO;
+import integrador2Arqui.DTO.ReporteDTO;
 import integrador2Arqui.clases.Carrera;
 import integrador2Arqui.clases.Estudiante;
 
@@ -9,4 +10,5 @@ public interface RepoEstudianteCarrera {
 	public void insert(Estudiante estudiante, Carrera carrera);	//matricula un estudiante a una carrera
 	public CarreraConInscriptosDTO getCarrerasConInscriptos();	/**ORDENAR POR CANT. INSCRIPTOS**/
 	public EstudianteDTO getEstudiantesByCarrera(Carrera carrera); /**FILTRAR POR CIUDAD DE RESIDENCIA**/
+	public ReporteDTO getReporteCarreras(); /**ordenar las carreras alfabéticamente, y presentar los años de manera cronológica**/
 }
