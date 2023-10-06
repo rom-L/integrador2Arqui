7dfc12e64092c611c214ddc302591cac50e2664e
rom-L <larrondoromeo@gmail.com>
2023-10-06 20:45:47 -0300

    identacion

--- interfaces/RepoCarrera.java
+++ interfaces/RepoCarrera.java
@@ -1,16 +1,10 @@
 package integrador2Arqui.interfaces;
 
-import java.util.List;
 import integrador2Arqui.clases.Carrera;
 
 public interface RepoCarrera {
-
 	public void insert(Carrera c);
-
 	public boolean delete(int id);
-
 	public boolean update(Carrera c);
-
 	public Carrera get(int id);
-
 }
