152f6af1a5798562daf5ad4d239716a43fefcf14
rom-L <larrondoromeo@gmail.com>
2023-10-07 12:01:50 -0300

    creado metodos generales repo

--- repositorios/RepoCarreraMySQL.java
+++ repositorios/RepoCarreraMySQL.java
@@ -1,5 +1,41 @@
 package integrador2Arqui.repositorios;
 
-public class RepoCarreraMySQL {
+import integrador2Arqui.clases.Carrera;
+import integrador2Arqui.interfaces.RepoCarrera;
+import jakarta.persistence.EntityManager;
 
+public class RepoCarreraMySQL implements RepoCarrera {
+	private EntityManager manager;
+	
+	public RepoCarreraMySQL(EntityManager manager) {
+		this.manager = manager;
+	}
+
+	
+	@Override
+	public void insert(Carrera c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean delete(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Carrera c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Carrera get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
 }
--- repositorios/RepoEstudianteCarreraMySQL.java
+++ repositorios/RepoEstudianteCarreraMySQL.java
@@ -1,5 +1,42 @@
 package integrador2Arqui.repositorios;
 
-public class RepoEstudianteCarreraMySQL {
+import integrador2Arqui.DTO.CarreraConInscriptosDTO;
+import integrador2Arqui.DTO.EstudianteDTO;
+import integrador2Arqui.DTO.ReporteDTO;
+import integrador2Arqui.clases.Carrera;
+import integrador2Arqui.clases.Estudiante;
+import integrador2Arqui.interfaces.RepoEstudianteCarrera;
+import jakarta.persistence.EntityManager;
 
+public class RepoEstudianteCarreraMySQL implements RepoEstudianteCarrera {
+	private EntityManager manager;
+	
+	public RepoEstudianteCarreraMySQL(EntityManager manager) {
+		this.manager = manager;
+	}
+
+	
+	@Override
+	public void insert(Estudiante estudiante, Carrera carrera) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public CarreraConInscriptosDTO getCarrerasConInscriptos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EstudianteDTO getEstudiantesByCarrera(Carrera carrera) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReporteDTO getReporteCarreras() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
