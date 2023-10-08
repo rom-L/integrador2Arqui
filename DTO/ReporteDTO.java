81c2ec9d1121b32824e1ef271a5126233bfae459
Skpone <mulaterofranco@gmail.com>
2023-10-08 20:56:39 -0300

    .

--- DTO/ReporteDTO.java
+++ DTO/ReporteDTO.java
@@ -4,36 +4,34 @@ public class ReporteDTO {
     private CarreraDTO carrera;
     private int cantInscriptosPorAnio;
     private int egresadosPorAnio;
+    private int anio;
+    
+    
+	public CarreraDTO getCarrera() {
+		return carrera;
+	}
 
-    public ReporteDTO(CarreraDTO carrera, int cantInscriptosPorAnio, int egresadosPorAnio) {
-        this.carrera = carrera;
-        this.cantInscriptosPorAnio = cantInscriptosPorAnio;
-        this.egresadosPorAnio = egresadosPorAnio;
-    }
+
+	public int getCantInscriptosPorAnio() {
+		return cantInscriptosPorAnio;
+	}
 
 
-    public CarreraDTO getCarrera() {
-        return carrera;
-    }
+	public int getEgresadosPorAnio() {
+		return egresadosPorAnio;
+	}
 
-    public int getCantInscriptos() {
-        return cantInscriptosPorAnio;
-    }
 
-    public void setCantInscriptos(int cantInscriptosPorAnio) {
-        this.cantInscriptosPorAnio = cantInscriptosPorAnio;
-    }
+	public int getAnio() {
+		return anio;
+	}
 
-    public int getEgresadosPorAno() {
-        return egresadosPorAnio;
-    }
 
-    public void setEgresadosPorAno(int egresadosPorAno) {
-        this.egresadosPorAnio = egresadosPorAno;
-    }
+	@Override
+	public String toString() {
+		return "ReporteDTO [carrera=" + carrera + ", cantInscriptosPorAnio=" + cantInscriptosPorAnio
+				+ ", egresadosPorAnio=" + egresadosPorAnio + ", anio=" + anio + "]";
+	}
 
-    @Override
-    public String toString() {
-        return this.carrera + "| Inscriptos: " + this.cantInscriptosPorAnio + "| Egresados por año: " + this.egresadosPorAnio;
-    }
+    
 }
