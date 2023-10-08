4b118df76a10ba61ce1311af12745178c9d84a95
rom-L <larrondoromeo@gmail.com>
2023-10-08 18:04:10 -0300

    getters y setters estudianteDTO

--- DTO/EstudianteDTO.java
+++ DTO/EstudianteDTO.java
@@ -20,5 +20,60 @@ public class EstudianteDTO {
         this.numeroLibreta = numeroLibreta;
     }
 
+    
+    public int getDni() {
+        return dni;
+    }
 
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCiudadResidencia() {
+        return ciudadResidencia;
+    }
+
+    public void setCiudadResidencia(String ciudadResidencia) {
+        this.ciudadResidencia = ciudadResidencia;
+    }
+
+    public String getNumeroLibreta() {
+        return numeroLibreta;
+    }
+
+    public void setNumeroLibreta(String numeroLibreta) {
+        this.numeroLibreta = numeroLibreta;
+    }
 }
